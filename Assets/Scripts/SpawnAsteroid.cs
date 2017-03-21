46207f810c3987ddfbc0dd6783680ab7fff59619
Essi Jukkala <essi.jukkala@aalto.fi>
2017-03-21 22:03:50 +0200

    turret looks into spawned asteroid

---
+++ Assets/Scripts/SpawnAsteroid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroid : MonoBehaviour {
+
+	public GameObject asteroid;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(asteroid, new Vector2(Random.Range(-5,5), Random.Range(-5,5)), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TurretControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour {
+	GameObject[] asteroids;
+	GameObject asteroid;
+
+	void Start () {
+		asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+		asteroid = asteroids[0];
+	}
+	
+	void Update () {
+		Vector3 dir = asteroid.transform.position - gameObject.transform.position;
+		//Matf.Atan2: return value is the angle between the x-axis 
+		//and a 2D vector starting at zero and terminating at (x,y).
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		//AngleAxis: Creates a rotation which rotates angle degrees around axis.
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+}
