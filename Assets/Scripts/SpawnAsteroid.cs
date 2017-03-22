630a549a9e6c1bdc6493ce13bd07b29d7da9331b
Essi Jukkala <essi.jukkala@aalto.fi>
2017-03-22 13:14:05 +0200

    turret now looks to closest asteroid

--- Assets/Scripts/SpawnAsteroid.cs
+++ Assets/Scripts/SpawnAsteroid.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class SpawnAsteroid : MonoBehaviour {
 
 	public GameObject asteroid;
+	public static int asteroidCount=0;
 
-	// Use this for initialization
 	void Start () {
-		Instantiate(asteroid, new Vector2(Random.Range(-5,5), Random.Range(-5,5)), Quaternion.identity);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		//always have 5 asteroids
+		if(asteroidCount<5){
+			GameObject asteroidInstance = Instantiate(asteroid, new Vector3(RandomPosX(), RandomPosY(), 0), Quaternion.identity);
+			asteroidCount++;
+		}
+	}
+
+	//-5 to -2.5, +2.5 +5, not on the safe zone 
+	float RandomPosY(){
+		float posy = Random.Range(2.5f,7.5f);
+		//map values > 5 to range -5 to -2.5
+		Debug.Log(posy);
+ 		if (posy > 5)
+     		posy = 2.5f-posy;
+
+     	return posy;
+	}
+
+	//-9 to -2.5, +2.5 to 9, not on the safe zone
+	float RandomPosX(){
+		float posx = Random.Range(2.5f,11.5f);
+		Debug.Log(posx);
+
+ 		if (posx > 9)
+     		posx = 7.5f-posx;
+
+     	return posx;
 	}
 }
--- Assets/Scripts/TurretControl.cs
+++ Assets/Scripts/TurretControl.cs
@@ -7,16 +7,35 @@ public class TurretControl : MonoBehaviour {
 	GameObject asteroid;
 
 	void Start () {
-		asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
-		asteroid = asteroids[0];
+		
 	}
 	
 	void Update () {
+		asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+
+		//find the closest TODO: update to next hit
+		asteroid = closest(asteroids);
+
 		Vector3 dir = asteroid.transform.position - gameObject.transform.position;
 		//Matf.Atan2: return value is the angle between the x-axis 
 		//and a 2D vector starting at zero and terminating at (x,y).
 		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 		//AngleAxis: Creates a rotation which rotates angle degrees around axis.
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	GameObject closest(GameObject[] asteroids){
+	    GameObject c = null;
+	    float minDist = Mathf.Infinity;
+	    Vector3 currentPos = gameObject.transform.position;
+	    foreach (GameObject a in asteroids)
+	    {
+	        float dist = Vector3.Distance(a.transform.position, currentPos);
+	        if (dist < minDist){
+	            c = a;
+	            minDist = dist;
+	        }
+	    }
+	    return c;
 	}
 }
