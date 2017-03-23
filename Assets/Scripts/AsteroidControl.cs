94632b904dcd09710beb1011816c9ffeb2e7ac90
Essi Jukkala <essi.jukkala@aalto.fi>
2017-03-23 13:17:13 +0200

    asteroids can be moved with mouse

--- Assets/Scripts/AsteroidControl.cs
+++ Assets/Scripts/AsteroidControl.cs
@@ -6,23 +6,44 @@ public class AsteroidControl : MonoBehaviour {
 	public int id;
 	public bool willHit = false;
 	public GameObject explosion;
-	
+	float x,y;
+
 	void Start(){
-		willHit = TrajectoryWithinSafetyZone(transform.position, GetComponent<Rigidbody2D>().velocity);
-		if(!willHit)
-			gameObject.GetComponent<Collider2D>().enabled=false;
 	}
 
 	void Update () {
 
 		Physics2D.IgnoreLayerCollision(8,8,true);
 		Physics2D.IgnoreLayerCollision(8,9,true);
+		Physics2D.IgnoreLayerCollision(11,8,true);
+		Physics2D.IgnoreLayerCollision(11,9,true);
+		Physics2D.IgnoreLayerCollision(11,10,true);
+		Physics2D.IgnoreLayerCollision(11,11,true);
 
 		if(transform.position.x > 10 || transform.position.x < -10 
 			|| transform.position.y > 6 || transform.position.y < -6){
 			Destroy(gameObject);
 			SpawnAsteroid.asteroidCount--;
 		}
+
+		willHit = TrajectoryWithinSafetyZone(transform.position, GetComponent<Rigidbody2D>().velocity);
+		if(!willHit)
+			gameObject.layer = 11;
+		else
+			gameObject.layer = 8;
+
+		x = Input.mousePosition.x;
+    	y = Input.mousePosition.y;
+	}
+	void OnMouseDrag(){
+    	transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
+	}
+
+	void OnMouseUp() {
+		Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 dir = MousePos - transform.position;
+		dir.Normalize ();
+		GetComponent<Rigidbody2D>().velocity = dir * 4f;
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
